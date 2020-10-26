d15c85ab36c9807ec15b1fe0872be1e54754f9c6
Anil kumar.Kanchamreddy <73389571+anilkanchamreddy@users.noreply.github.com>
2020-10-26 12:03:27 +0530

    Update range_function.py

--- range_function.py
+++ range_function.py
@@ -1,11 +1,11 @@
 #sequence of numbers from 0 to 9
 for i in range(0,10):
-print(i)
+  print(i)
 
 #sequences of numbers from 0 to 9 with step 1
 for i in range(0,10,1):
-print(i,end=' ')
+  print(i,end=' ')
 
 #sequences of numbers from 0 to 9 with step -1 i.e(9 to 0)
-for i in range(0,10,-1)
-print(i)
+for i in range(0,10,-1):
+  print(i)
